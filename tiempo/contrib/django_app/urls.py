f9d4e92bfffd4f6b2fcd5bacf966ab2133f87169
Damon C <gdamon@gmail.com>
2016-08-22 19:50:17 -0400

    updates urls for 1.10 compat

--- tiempo/contrib/django_app/urls.py
+++ tiempo/contrib/django_app/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'tiempo.contrib.django_app.views',
-    url(r'^$', 'dashboard', name='tiempo_dashboard'),
-    url(r'^all_tasks/$', 'all_tasks', name='all_tiempo_tasks'),
-    url(r'^recent/$', 'recent_tasks', name='recent_tasks'),
-    url(r'^results/(?P<key>.+)', 'results', name='task_results')
-)
+from tiempo.contrib.django_app.views import dashboard, all_tasks, recent_tasks, results
+
+urlpatterns = [
+    url(r'^$', dashboard, name='tiempo_dashboard'),
+    url(r'^all_tasks/$', all_tasks, name='all_tiempo_tasks'),
+    url(r'^recent/$', recent_tasks, name='recent_tasks'),
+    url(r'^results/(?P<key>.+)', results, name='task_results')
+]
+
